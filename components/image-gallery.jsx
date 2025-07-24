ba9716ddd5f21987c57e50e100af3c3fd19016b7
theflasia <theflasia@gmail.com>
2025-07-24 22:58:06 +0900

    Change image gallery cols size

--- components/image-gallery.jsx
+++ components/image-gallery.jsx
@@ -18,7 +18,7 @@ export default function ImageGallery({ images = [] }) {
 
   return (
     <div>
-      <div className="grid grid-cols-3 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5">
+      <div className="grid grid-cols-3 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5">
         {images.map((image, index) => (
           <div
             key={index}
